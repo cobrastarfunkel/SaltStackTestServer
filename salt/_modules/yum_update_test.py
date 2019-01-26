6d3d9401f8678b2901db225807c5d37928dd70a3
cobrastarfunkel <iancobia@gmail.com>
2019-01-26 17:25:44 -0600

    Module now handles the pushing and moving of files from the minions to the master cache and finally to the directory specified in the file.  Push is its own function

--- salt/_modules/yum_update_test.py
+++ salt/_modules/yum_update_test.py
@@ -26,7 +26,8 @@ __grains__ = salt.loader.grains(__opts__)
 
 # Alias for the run_updates module
 __func_alias__ = {
-    'run_updates': 'update'
+    'run_updates': 'update',
+    'push_files': 'push',
 }
 
 # Salt module can be called by this name
@@ -56,23 +57,34 @@ def _yum_test():
 
     if 'No packages' in yum_output:
         log.warning('No Packages marked for Update')
-        push_file = '/tmp/{}_{}_No_Packages'.format(__grains__['id'], time.strftime("%Y%m%d"))
+        push_file = '{}_{}_No_Packages'.format(__grains__['id'], time.strftime("%Y%m%d"))
         f = open(push_file, 'w')
         f.write(yum_output)
         return (False, push_file)
     
     elif ('failed' in yum_output) or ('error' in yum_output) or ('errno' in yum_output):
         log.error('#### Yum Cmd Failed! ####')
-        push_file = '/tmp/{}_{}_FAILED'.format(__grains__['id'], time.strftime("%Y%m%d"))
+        push_file = '{}_{}_FAILED'.format(__grains__['id'], time.strftime("%Y%m%d"))
         f = open(push_file, 'w')
         f.write(yum_output)
         return (False, push_file)
     
     else:
-        push_file = '/tmp/{}_{}_Succeeded'.format(__grains__['id'], time.strftime("%Y%m%d"))
+        push_file = '{}_{}_Succeeded'.format(__grains__['id'], time.strftime("%Y%m%d"))
         f = open(push_file, 'w')
         f.write(yum_output)
         return (True, push_file)
+
+
+
+def push_files(push_file, file_path):
+       __salt__['cp.push']('/tmp/{}'.format(push_file), remove_source=True)
+       
+       # Move the files from salt cache
+       _sp.call(
+       'find /var/cache/salt/master/ -name \'%s\' -exec mv -t %s {} +' 
+       % (push_file, file_path), shell=True)
+
 
 
 def run_updates():
@@ -85,24 +97,34 @@ def run_updates():
     CLI Example:
         salt "*" yum.update
     '''
-    if not os.path.exists('/tmp/updated_minions'):
-        _sp.call('mkdir /tmp/updated_minions', shell=True)
 
+    # Path where you want the Log from the update to be stored
+    update_file_path = '/home/update_minions'
+
+
+    if not os.path.exists(update_file_path):
+        _sp.call('mkdir {}'.format(update_file_path), shell=True)
+
+    # Run the yum update and save output
     update_succeeded, push_file = _yum_test()
+    
+    # Push the files from minion to master
+    push_files(push_file, update_file_path)
 
     if update_succeeded:
-       __salt__['cp.push'](push_file, remove_source=True)
        __salt__['event.fire_master']('{"Update":"Succeeded"}', '/update/complete')
        # TODO: _sp.call("reboot", shell=True)
-       return (True, 'Update Run and files pushed to master Rebooting.....')
+       return (
+       'Update Run and files pushed to master Rebooting.....', 
+       'Log located in {} '.format(update_file_path))
 
     elif 'No_Packages' in push_file:
-      __salt__['cp.push'](push_file, remove_source=True)
-      __salt__['event.fire_master']('{"Update":"Succeeded"}', '/update/complete')
-      return (True, 'No Packages Marked for Update')
+      __salt__['event.fire_master']('{"Update":"NoPacks"}', '/update/nopacks')
+      return (
+      'No Packages Marked for Update', 
+      'Log located in {} '.format(update_file_path))
 
     else:
-      __salt__['cp.push'](push_file, remove_source=True)
       __salt__['event.fire_master']('{"Update":"Failed"}', '/update/complete')
-      return (False, 'Check Minion Log or /tmp/updated_minions/ for update Error')
+      return (False, 'Check Minion Log or {} for update Error'.format(update_file_path))
 
